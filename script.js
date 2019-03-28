8cc35012f8f586468867f27a21484c3f06ebf064
1203CakeoSmakeo <43444272+1203CakeoSmakeo@users.noreply.github.com>
2019-03-28 09:51:15 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+$("button").click(function() {
+    var Tyrant= $("#15").val();
+    console.log("15");
+});
+$("button").click(function() {
+    var Tyrant= $("#Tyrant").val();
+    console.log("Tyrant");
+});
+$("button").click(function() {
+    var Tyrant= $("#Tyrant").val();
+    console.log("Tyrant");
+});
+$("button").click(function() {
+    var Tyrant= $("#Tyrant").val();
+    console.log("Tyrant");
+});
+$("button").click(function() {
+    var Tyrant= $("#Tyrant").val();
+    console.log("Tyrant");
+});
